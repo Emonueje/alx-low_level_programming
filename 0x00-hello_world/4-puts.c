157a5bbbe7c696dd49ee7b2d399fb154036eec7b
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-02-17 01:17:09 +0000

    my eyes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-#include <stdio.h>
+#include  <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle\n";
-	
-	puts(str);
+	puts("\"Programming is like building a mulitlingual puzzle");
 	return (0);
 }
