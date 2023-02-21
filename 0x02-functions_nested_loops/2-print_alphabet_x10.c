6d45622c63022f28b314733bc5eba1a6e4db3494
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-02-21 21:49:19 +0000

    Project 10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print the alphabets 10x
+ *
+ * Return: No value
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char p;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (p = 'a'; p <= 'z'; p++)
+		{
+			_putchar(p);
+		}
+	_putchar('\n');
+	}
+}
