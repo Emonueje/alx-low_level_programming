f99f462f7ca2708a96bb4105d9907b3fea471b00
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-02-21 21:33:54 +0000

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,17 +5,6 @@
  *
  * Return: Always 0
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
- * print_alphabet -usable function
- *
- * Return: Always 0
- */
 void print_alphabet(void)
 {
 	char a;
