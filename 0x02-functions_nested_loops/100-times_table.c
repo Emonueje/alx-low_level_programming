3596d01c6ed777c603b104499df031cd042d0dcf
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-02-22 19:14:30 +0000

    Needed that song

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_times_table - outputs the xtables.
+ * @n: the input value for processing.
+ * Return: VOid functions return no value.
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n  > 15 || n < 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+				if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k  + '0');
+				}
+				else if (k >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
