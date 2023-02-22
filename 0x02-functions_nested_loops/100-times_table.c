8d55b74e0b767ab59caf9af2a2dd7f3168aab309
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-02-22 19:44:36 +0000

    Commit update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n  > 15 || n < 0)
+	if ((n  > 15) || (n < 0))
 		_putchar('\n');
 	else
 	{
@@ -21,18 +21,17 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + '0');
+					_putchar(',');
 				}
 				if (k < 10 && j != 0)
 				{
-					_putchar(',');
-					_putchar(' ');
+					_putchar(' ');	
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k  + '0');
 				}
 				else if (k >= 10 && k < 100)
 				{
-					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
@@ -40,7 +39,6 @@ void print_times_table(int n)
 				}
 				else if (k > 99)
 				{
-					_putchar(',');
 					_putchar(' ');
 					_putchar(((k / 10) / 10) + '0');
 					_putchar(((k / 10) % 10) + '0');
