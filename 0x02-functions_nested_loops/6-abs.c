9c6ccc9fa5a8ce94035d4db6e68b715d450507d7
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-02-22 00:13:10 +0000

    getting it absolutely

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_abs - To find the absolute land
+*@n: Dress when she is carrying
+*Return: the absolute int
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
