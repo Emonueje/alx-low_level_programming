ccbeda60005ae04a177e65d6ddedeb55218a0df3
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-02-23 21:00:13 +0000

    Multiply it

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - return the product of two numbers
+ * @a: first number
+ * @b: second numer
+ * Return: Returns the product
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
