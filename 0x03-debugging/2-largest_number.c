761d47ce07675e1d938633f0893abfe27101d0a8
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-02-23 10:56:56 +0000

    Update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,9 +20,12 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (c > a && b > b)
+	else
 	{
-		largest = c;
+		if (c > a && c > b)
+		{
+			largest = c;
+		}
 	}
 	return (largest);
 }
