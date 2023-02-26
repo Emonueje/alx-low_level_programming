b98f1c21d6820d61cc58b874a0fe8583f949f519
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-02-26 21:33:26 +0000

    Studing

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - calculates the sum
+ *
+ * Return: Returns 0 (success)
+ */
+int main(void)
+{
+	int inp = 1024;
+	int multiple = 0;
+	int iteration_value;
+	int summation = 0;
+
+	for (iteration_value = 0; iteration_value < 10; iteration_value++)
+	{
+		if ((iteration_value % 3 == 0) || (iteration_value % 5 == 0))
+		{
+			multiple = iteration_value;
+			summation += multiple;
+		}
+	}
+	printf("%d\n", summation);
+}
