335664ed43ed10f99504af044e03a0cf0df6c454
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-02-27 20:45:36 +0000

    Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points to.
+ * @*n: pointer of the input
+ * @n: holds the address of the input
+ * Return: No value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
