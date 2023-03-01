cc9fae534ec69347c3b133fa4b1b9e2435eeb9a5
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-03-01 15:55:48 +0000

    Update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * puts2 - prints everyother character of a string
@@ -12,12 +13,7 @@ void puts2(char *str)
 	char *c = str;
 	int i;
 
-	while (*c != '\0')
-	{
-		c++;
-		l++;
-	}
-	t = l - 1;
+	t = strlen(str) -1;
 	for (i = 0; i <= t; i++)
 	{
 		if (i % 2 == 0)
