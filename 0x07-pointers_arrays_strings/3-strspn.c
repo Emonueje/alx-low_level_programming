42ef10684591eb725a526e1ce999e67586e74f3f
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-03-06 13:31:16 +0000

    update

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strspn - gets length of a prefix substring
+ * @s: input pointer to string
+ * @accept: input pointer to strin
+ * Return: Returns an unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+	`
+}
