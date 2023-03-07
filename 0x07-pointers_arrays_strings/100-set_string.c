d40d5083b086df980aa417a063773da5aca74cc4
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-03-07 13:56:21 +0000

    Double pointer double fan

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: input pointer
+ * @to: input pointer
+ * Return: No value returned for void functions
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+	
+}
