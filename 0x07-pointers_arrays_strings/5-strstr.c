f22be493a9137f9ce822da24d36f3163562b7f38
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-03-07 12:42:34 +0000

    Where is it located

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,16 +1,21 @@
 #include "main.h"
 #include <stddef.h>
+
 /**
  * _strstr - locates a substring
- * @haystack: input string
- * @needle: input string
- * Return: Returns a pointer of char to function
+ * @haystack: input
+ * @needle: input
+ * Return: A pointer to the function
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i, j;
-	char *temp = " ";
-
-	for (i = 0; haystack[i]; i++)
+	while(*haystack)
 	{
+		if (*haystack == *needle)
+			return(haystack);
+		if (!haystack)
+			return (NULL);
+		haystack++;
+	}
+	return (NULL);
 }
