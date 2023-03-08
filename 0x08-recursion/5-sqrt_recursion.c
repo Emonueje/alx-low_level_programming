a528018b4e246ab7151740d8af9ce6b55cf475c3
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-03-08 19:27:27 +0000

    get the sqrt recursively

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,21 @@
 #include "main.h"
 
+
+/**
+ * get_sqrt - gets the sqrt
+ * @num: input integer
+ * @tmp: input trial
+ * Return: an the exact num
+ */
+int get_sqrt(int num, int tmp)
+{
+	if (tmp * tmp > num)
+		return (-1);
+	if (tmp * tmp == num)
+		return (tmp);
+	return (get_sqrt(num, (tmp + 1)));
+}
+
 /**
  * _sqrt_recursion - finds the sqrt of input
  * @n: input of type int
@@ -7,10 +23,5 @@
  */
 int _sqrt_recursion(int n)
 {
-
-	if ((n / (n - 1) == (n - 1))
-		return (n);
-	else
-		return (i / (_sqrt_recursion(n - 1)));
-	return (-1);
+	return (get_sqrt(n, 1));
 }
