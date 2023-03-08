6f6b6adb3ac70f0b51c99b44625545017ec150dc
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-03-08 20:58:24 +0000

    Is prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * getprime - gets the prime number
+ * @num: input
+ * @tmp: input
+ * Return: an int, 1 if prime and 0 if not
+ */
+int getprime(int num, int tmp)
+{
+	if (num <= 1)
+		return (0);
+	if (num == tmp)
+		return (1);
+	else
+	{
+		if (num % tmp == 0)
+			return (0);
+		else
+			return (getprime(num, tmp + 1));
+	}
+}
+
+/**
+ * is_prime_number - checks if prime number
+ * @n: input
+ * Return: 1 if prime and 0 if not
+ */
+int is_prime_number(int n)
+{
+	return (getprime(n, 2));
+}
