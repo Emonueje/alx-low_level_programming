a0b9598c1ff43a4f46cdeddc964d2573f4dbd204
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-03-08 15:23:35 +0000

    squareroot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - finds the sqrt of input
+ * @n: input of type int
+ * Return: the sqrt of the number
+ */
+int _sqrt_recursion(int n)
+{
+
+	if ((n / (n - 1) == (n - 1))
+		return (n);
+	else
+		return (i / (_sqrt_recursion(n - 1)));
+	return (-1);
+}
