911bfdea35387e6ab38638c76f87e9cf6b37d8ce
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-03-08 13:07:05 +0000

    Begin with the last then to the first

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,4 +3,12 @@
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: input string
- * Return: 
+ * Return: void function, No return value
+ */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s)
+		_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
