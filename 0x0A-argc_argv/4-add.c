695d1be0df8694ef099769b1c18a2f968063ab4c
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-03-14 18:21:29 +0000

    Updating and fixing

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * isInt - checks for Numbers
+ * @str: input string
+ * Return: 0 on success and 1 on error
+ */
+int isInt(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (!(isdigit(str[i])))
+			return (1);
+	}
+	return (0);
+}
 
 /**
  * main - Entry point
@@ -19,7 +37,7 @@ int main(int argc, char **argv)
 			printf("%d\n", sum);
 			break;
 		}
-		if (atoi(argv[i]))
+		if (!(isInt(argv[i])))
 		{
 			sum += atoi(argv[i]);
 		}
